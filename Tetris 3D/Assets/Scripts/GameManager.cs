ebee2564f702a011598a5f47e519092d75f31a8a
Iago Lobo <iago.wolf.moraes@gmail.com>
2020-12-31 01:53:56 -0300

    Core gameplay mechanics implemented
    7 tetris blocks, block movement and rotation, clear row and adding score

--- Tetris 3D/Assets/Scripts/GameManager.cs
+++ Tetris 3D/Assets/Scripts/GameManager.cs
@@ -1,20 +1,123 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    private int gridHeight = 20;
+    private int gridWidth = 10;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject[] tetrisBlocks;
+    private Transform[,] grid = new Transform[10, 20];
+
+    private int currentScore;
+    [SerializeField] private TextMeshProUGUI currentScoreText;
+
+    public int GridHeight
     {
-        
+        get {   return gridHeight;   }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int GridWidth
     {
-        
+        get {   return gridWidth;   }
+    }
+
+    public Transform[,] Grid
+    {
+        get {   return grid;    }
+    }
+
+    private void Awake()
+    {
+        if(Instance == null)    Instance = this;
+    }
+
+    private void Start()
+    {
+        currentScore = 0;
+        UpdateCurrentScore(currentScore);
+        SpawnNewBlock();
+    }
+
+    public void SpawnNewBlock()
+    {
+        int randomID = Random.Range(0, tetrisBlocks.Length);
+        Instantiate(tetrisBlocks[randomID], spawnPoint.position, Quaternion.identity);
+    }
+
+    public void AddTetrisBlockToGrid(GameObject tetrisBlock)
+    {
+        foreach(Transform block in tetrisBlock.transform)
+        {
+            int xInt = Mathf.RoundToInt(block.transform.position.x);
+            int yInt = Mathf.RoundToInt(block.transform.position.y);
+
+            grid[xInt, yInt] = block;
+        }
+    }
+
+    public void CheckRowCleared()
+    {
+        for(int j = gridHeight - 1; j >= 0; --j)
+        {
+            if(IsRowFull(j))
+            {
+                ClearRow(j);
+                BringRowsDown(j);
+                AddScore();
+            }
+        }
+    }
+
+    private bool IsRowFull(int j)
+    {
+        for(int i = 0; i < gridWidth; ++i)
+        {
+            if(grid[i,j] == null) return false;
+        }
+
+        return true;
+    }
+
+    private void ClearRow(int j)
+    {
+        for(int i = 0; i < gridWidth; ++i)
+        {
+            Destroy(grid[i,j].gameObject);
+            grid[i,j] = null;
+        }
+    }
+
+    private void BringRowsDown(int j)
+    {
+        for(int y = j; y < gridHeight; y++)
+        {
+            for(int i = 0; i < gridWidth; i++)
+            {
+                if(grid[i,y] != null)
+                {
+                    grid[i, y-1] = grid[i, y];
+                    grid[i, y] = null;
+                    grid[i, y-1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    private void AddScore()
+    {
+        currentScore += 10;
+        UpdateCurrentScore(currentScore);
+    }
+
+    private void UpdateCurrentScore(int score)
+    {
+        currentScoreText.text = "Score: \n" + $"{score}";
     }
 
     public void GoToMainMenuButton()
---
+++ Tetris 3D/Assets/Scripts/TetrisBlock.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    public Vector3 rotationPoint;
+    private float lastTimeBlockFell = 0f;
+    private float fallTimer = 1f;
+
+    private void Update()
+    {
+        MoveBlock();
+        BlockFallingDown();
+    }
+
+    private void MoveBlock()
+    {
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if(!IsMoveValid())  transform.position -= new Vector3(-1, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if(!IsMoveValid())  transform.position -= new Vector3(1, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            RotateBlock();
+        }
+    }
+
+    private void BlockFallingDown()
+    {
+        lastTimeBlockFell += Time.deltaTime;
+        if(lastTimeBlockFell >
+        ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) ? fallTimer / 20 : fallTimer))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            lastTimeBlockFell = 0f;
+
+            // If the block can't go down anymore, another block needs to spawn and this block needs to stop
+            if(!IsMoveValid())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                GameManager.Instance.AddTetrisBlockToGrid(this.gameObject);
+                GameManager.Instance.CheckRowCleared();
+                GameManager.Instance.SpawnNewBlock();
+                this.enabled = false;
+            }
+        }
+    }
+
+    private void RotateBlock()
+    {
+        Vector3 globalRotationPoint = transform.TransformPoint(rotationPoint);
+        transform.RotateAround(globalRotationPoint, new Vector3(0, 0, 1), -90);
+        if(!IsMoveValid())  transform.RotateAround(globalRotationPoint, new Vector3(0, 0, 1), 90);
+    }
+
+    private bool IsMoveValid()
+    {
+        foreach(Transform block in transform)
+        {
+            int xInt = Mathf.RoundToInt(block.transform.position.x);
+            int yInt = Mathf.RoundToInt(block.transform.position.y);
+
+            if(xInt < 0 || xInt >= GameManager.Instance.GridWidth || yInt < 0 || yInt >= GameManager.Instance.GridHeight)
+                return false;
+
+            if(GameManager.Instance.Grid[xInt, yInt] != null)   return false;
+        }
+
+        return true;
+    }
+}
